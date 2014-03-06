b596bd4131a73bc9748b70dcea4467fd30f31db2
Jeremy Crawford <jcrawford.design@gmail.com>
2014-03-06 14:30:08 -0500

    Made changes in index

---
+++ app/scripts/burger.js
@@ -0,0 +1,5 @@
+var Burger = Parse.Object.extend("Burger")
+ 
+var BurgersCollection = Parse.Collection.extend({
+  model: Burger
+})
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
 console.log('\'Allo \'Allo!');
+ 
+$(function(){
+  Parse.initialize("niea3eYLacFwpITMMHZf2pl9TVKRGtR4q6VNRDHx", "hNIHE3gc2yfnumjZ71PtsN8FjKUYSdoBGbbDtsG4");
+ 
+  // var pizzaBurger = new Burger;
+  // pizzaBurger.set('breadType', 'soggyish')
+  // pizzaBurger.save({name: 'Pizza Burger'})
+ 
+  window.burgers = new BurgersCollection();
+  burgers.fetch();
+})
